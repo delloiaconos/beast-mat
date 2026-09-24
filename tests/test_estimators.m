c167377a5fffc233c785660cd4ce3634a80e9dba
Salvatore Dello Iacono <delloiaconos@gmail.com>
2026-09-24 10:50:02 +0200

    Added standard header to files.

--- tests/test_estimators.m
+++ tests/test_estimators.m
@@ -1,5 +1,21 @@
-function results = test_estimators( results )
+% BEAST - Battery Estimation Algorithms and Simulation Toolkit
+%
+% This file is part of the BEAST MATLAB implementation.
+%
+% Project:
+%   Battery Estimation Algorithms and Simulation Toolkit (BEAST)
+%
+% Repository:
+%   https://github.com/delloiaconos/beast-mat
+%
+% Copyright (C) 2026 Salvatore Dello Iacono
+% SPDX-License-Identifier: GPL-3.0-or-later
+%
+% See the LICENSE file in the project repository for license information.
+%
+% NOTE: Detailed file documentation is to be added as the implementation matures.
 
+function results = test_estimators( results )
     % CONSTANTS
     myCoeffs = struct();
     myCoeffs.Qn_Ah  = 1.0;
